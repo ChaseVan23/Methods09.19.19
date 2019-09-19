1a0b7df23bcc9aaf6b4cd7dc4a2ad2a14cab2d98
0774775 <0774775@KELCE-LABS17-P.pittstate.edu>
2019-09-19 11:11:32 -0500

    methods

---
+++ Methods/src/main/java/Methods.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0774775
+ */
+public class Methods {
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+}
